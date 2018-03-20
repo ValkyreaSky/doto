d8aaa4dcebec38ac7d738a19cee372979d2358d8
Adam Pałaszyński <35331661+appalaszynski@users.noreply.github.com>
2018-03-20 13:15:43 +0100

    Add timestamps

--- src/containers/ListContainer/ListContainer.jsx
+++ src/containers/ListContainer/ListContainer.jsx
@@ -1,48 +1,93 @@
 import React, { Component } from 'react';
 import { TodoList, TodoAdd, TodoSearch } from 'Components';
+import uuid from 'uuid';
+import moment from 'moment';
+import { filterTodos } from '../../api/TodoApi';
 
 class ListContainer extends Component {
 	state = {
-		showCompleted: true,
+		showCompleted: false,
 		searchText: '',
 		todos: [
 			{
-				id: 1,
-				text: 'Walk the dog'
+				id: uuid(),
+				text: 'Walk the dog',
+				completed: false,
+				createdAt: 123,
+				completedAt: undefined
 			},
 			{
-				id: 2,
-				text: 'Clean the yard'
+				id: uuid(),
+				text: 'Clean the yard',
+				completed: true,
+				createdAt: 60,
+				completedAt: 600
 			},
 			{
-				id: 3,
-				text: 'Burn the house'
+				id: uuid(),
+				text: 'Burn the house',
+				completed: true,
+				createdAt: 1230,
+				completedAt: 2340
       },
       {
-        id: 4,
-        text: 'Play video games'
+        id: uuid(),
+				text: 'Play video games',
+				completed: false,
+				createdAt: 12323,
+				completedAt: undefined
       }
 		]
   }
   
   handleAddTodo = (text) => {
-    alert('New Todo', text);
+    this.setState({
+			todos: [
+				...this.state.todos,
+				{
+					id: uuid(),
+					text,
+					completed: false,
+					createdAt: moment().unix(),
+					completedAt: undefined
+				}
+			]
+		});
 	}
+
+	handleToggle = (id) => {		
+		const todosArray = [...this.state.todos];
+		const index = todosArray.findIndex((todo) => {
+			return todo.id === id
+		});
+		
+		todosArray[index].completed = !todosArray[index].completed;
+		if (todosArray[index].completed) {
+			todosArray[index].completedAt = moment().unix();
+		} else {
+			todosArray[index].completedAt = undefined;
+		}
+
+		this.setState({
+			todos: todosArray
+		});
+	};
 	
 	handleSearch = (showCompleted, searchText) => {
 		this.setState({
 			showCompleted,
-			searchText
+			searchText: searchText.toLowerCase()
 		});
 	}
 
 	render() {
-    const { todos } = this.state;    
+		const { todos, showCompleted, searchText } = this.state;    
+		const filteredTodos = filterTodos(todos, showCompleted, searchText);
 
 		return (
 			<div>
 				<TodoSearch onSearch={this.handleSearch} />
-        <TodoList todos={todos} />
+				<TodoList todos={filteredTodos} onToggle={this.handleToggle} />
 				<TodoAdd handleAddTodo={this.handleAddTodo} />
 			</div>
 		);
--- src/containers/ListContainer/ListContainer.test.jsx
+++ src/containers/ListContainer/ListContainer.test.jsx
@@ -8,4 +8,52 @@ describe('ListContainer', () => {
 
 		expect(wrapper.exists()).toBe(true);
 	});
+
+	test('Should add Todo to todos state on handleAddTodo', () => {
+		const wrapper = shallow(<ListContainer />);
+
+		wrapper.instance().handleAddTodo('Test');
+
+		const length = wrapper.state('todos').length;
+
+		expect(wrapper.state('todos')[length - 1].text).toBe('Test');
+		expect(typeof(wrapper.state('todos')[length - 1].createdAt)).toBe('number');
+		expect(typeof(wrapper.state('todos')[length - 1].completedAt)).toBe('undefined');
+	});
+
+	test('Should toggle completed value when handleToggle called', () => {
+		const todo = {
+			id: 12,
+			text: 'Sample text',
+			completed: false,
+			createdAt: 100,
+			completedAt: undefined
+		};
+
+		const wrapper = shallow(<ListContainer />);
+		wrapper.setState({todos: [todo]});
+
+		wrapper.instance().handleToggle(12);
+
+		expect(wrapper.state('todos')[0].completed).toBe(true);
+		expect(typeof(wrapper.state('todos')[0].completedAt)).toBe('number');
+	});
+
+	test('Should toggle completed value when handleToggle called', () => {
+		const todo = {
+			id: 12,
+			text: 'Sample text',
+			completed: true,
+			createdAt: 100,
+			completedAt: 200
+		};
+
+		const wrapper = shallow(<ListContainer />);
+		wrapper.setState({todos: [todo]});
+
+		wrapper.instance().handleToggle(12);
+
+		expect(wrapper.state('todos')[0].completed).toBe(false);
+		expect(typeof(wrapper.state('todos')[0].completedAt)).toBe('undefined');
+	});
 });
